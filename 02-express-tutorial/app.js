40bdc8811dfa0ca029a4f5513b474677d573275f
Fiamma no Jikukan <stavtxxx1@gmail.com>
2021-05-23 17:28:42 +0300

    built a server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,29 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    // home page
+    if (url === '/') {
+        res.writeHead(200, { 'content-trype': 'html' })
+        res.write('<h1>Home Page</h1>')
+        res.end()
+    }
+    // about page
+    else if (url === '/about') {
+        res.writeHead(200, { 'content-trype': 'html' })
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    // if nothing found
+     else {
+        res.writeHead(404, { 'content-trype': 'html' })
+        res.write('<h1> Sowwy, error, page not found </h1>')
+        res.end()
+    }
+});
+
+
+
+server.listen(5000)
+
