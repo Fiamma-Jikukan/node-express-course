200cf1dc24c7b8b2e41eed9152728fcd15cfd635
Fiamma no Jikukan <stavtxxx1@gmail.com>
2021-05-23 18:04:38 +0300

    added example app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,29 +1,52 @@
-const http = require('http')
+const http = require('http');
+const {readFileSync} = require('fs');
+
+const homePage = readFileSync('./index.html')
+// const homePage = readFileSync('./navbar-app/index.html')
+const homeStyle = readFileSync('./navbar-app/styles.css')
+const homeLogo = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 
 const server = http.createServer((req, res) => {
     const url = req.url
     // home page
     if (url === '/') {
-        res.writeHead(200, { 'content-trype': 'html' })
-        res.write('<h1>Home Page</h1>')
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(homePage)
         res.end()
     }
     // about page
     else if (url === '/about') {
-        res.writeHead(200, { 'content-trype': 'html' })
+        res.writeHead(200, { 'content-type': 'text/html' })
         res.write('<h1>About Page</h1>')
         res.end()
     }
+    // // style
+    // else if (url === '/styles.css') {
+    //     res.writeHead(200, { 'content-type': 'text/css' })
+    //     res.write(homeStyle)
+    //     res.end()
+    // }
+    // // Logo
+    // else if (url === '/logo.svg') {
+    //     res.writeHead(200, { 'content-type': 'image/svg+xml' })
+    //     res.write(homeLogo)
+    //     res.end()
+    // }
+    // // logic
+    // else if (url === '/browser-app.js') {
+    //     res.writeHead(200, { 'content-type': 'text/javascript' })
+    //     res.write(homeLogic)
+    //     res.end()
+    // }
     // if nothing found
      else {
-        res.writeHead(404, { 'content-trype': 'html' })
+        res.writeHead(404, { 'content-type': 'text/html' })
         res.write('<h1> Sowwy, error, page not found </h1>')
         res.end()
     }
 });
-
-
 
 server.listen(5000)
 
