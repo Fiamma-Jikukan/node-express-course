f5596ebd21b87fadef50970d39ea55e76b1aff94
Fiamma no Jikukan <stavtxxx1@gmail.com>
2021-05-30 10:49:42 +0300

    Next topic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,9 @@
 
 const express = require('express')
-// const path = require('path')
-const app = express();
+const app = express()
+const { products } = require('./data')
+
+
 
 
 
@@ -12,76 +14,31 @@ app.listen(5000, () => {
 
 
 
-// app.use(express.static('./public'))
-
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     .send('Sowwy not found')
-// });
-
 // app.get('/', (req, res) => {
-    //     res.status(200)
-    //         .sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-    // });
+//     res.send('<h1>home page</h1> <a href="/api/products">Products</a>')
+// })
 
-// app.get('/about', (req, res) => {
-//     res.status(200)
-//     .send('About Page')
-// });
+// app.get('/api/products', (req, res) => {
+//     const newProducts = products.map((product) => {
+//         const { id, name, image } = product;
+//         return { id, name, image };
+//     })
+//     res.json(newProducts)
+// })
 
+// app.get('/api/products/:productID', (req, res) => {
+//     const { productID } = req.params;
 
-
-
-
-// const http = require('http');
-// const {readFileSync} = require('fs');
-
-// const homePage = readFileSync('./index.html')
-// // const homePage = readFileSync('./navbar-app/index.html')
-// const homeStyle = readFileSync('./navbar-app/styles.css')
-// const homeLogo = readFileSync('./navbar-app/logo.svg')
-// const homeLogic = readFileSync('./navbar-app/browser-app.js')
-
-
-// const server = http.createServer((req, res) => {
-//     const url = req.url
-//     // home page
-//     if (url === '/') {
-//         res.writeHead(200, { 'content-type': 'text/html' })
-//         res.write(homePage)
-//         res.end()
+//     const singleProduct = products.find((product) =>
+//         product.id === Number(productID));
+//     if (!singleProduct) {
+//         res.status(404).send("not here sowwy")
 //     }
-//     // about page
-//     else if (url === '/about') {
-//         res.writeHead(200, { 'content-type': 'text/html' })
-//         res.write('<h1>About Page</h1>')
-//         res.end()
-//     }
-//     // // style
-//     // else if (url === '/styles.css') {
-//     //     res.writeHead(200, { 'content-type': 'text/css' })
-//     //     res.write(homeStyle)
-//     //     res.end()
-//     // }
-//     // // Logo
-//     // else if (url === '/logo.svg') {
-//     //     res.writeHead(200, { 'content-type': 'image/svg+xml' })
-//     //     res.write(homeLogo)
-//     //     res.end()
-//     // }
-//     // // logic
-//     // else if (url === '/browser-app.js') {
-//     //     res.writeHead(200, { 'content-type': 'text/javascript' })
-//     //     res.write(homeLogic)
-//     //     res.end()
-//     // }
-//     // if nothing found
-//      else {
-//         res.writeHead(404, { 'content-type': 'text/html' })
-//         res.write('<h1> Sowwy, error, page not found </h1>')
-//         res.end()
-//     }
-// });
+//     return res.json(singleProduct)
+// })
 
-// server.listen(5000)
+// app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+//     console.log(req.params)
+//     res.send('hello world')
+//   })
 
